fe3d80547c8d5c070144a06154526273951cb878
JosephW <115873093+JosephWSVRP@users.noreply.github.com>
2025-05-20 10:00:46 +1000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
+author 'Joseph\'s Development'
+description 'Similar to the Context Menu resource from LSPDFR, just for FiveM.'
+version '1.0'
 
 client_scripts {
     '@NativeUI/NativeUI.lua', -- Ensure this path is correct
